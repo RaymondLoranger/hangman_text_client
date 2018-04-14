5c9c0daae1a256a8df55a9bc6839a509cbc0de3a
RaymondLoranger <Raymond.Loranger@bell.net>
2018-04-14 09:14:22 -0400

    0.1.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Hangman.TextClient.Mixfile do
     [
       app: :hangman_text_client,
       version: "0.1.1",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [plt_add_apps: [:hangman_engine]]
@@ -26,6 +26,7 @@ defmodule Hangman.TextClient.Mixfile do
       {:mix_tasks,
        github: "RaymondLoranger/mix_tasks", only: :dev, runtime: false},
       {:hangman_engine, github: "RaymondLoranger/hangman_engine"},
+      {:persist_config, "~> 0.1"},
       {:earmark, "~> 1.0", only: :dev},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:dialyxir, "~> 0.5", only: :dev, runtime: false}
