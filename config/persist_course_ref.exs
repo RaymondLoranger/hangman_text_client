34fc5943a1bda659deaa7f051fa4a1cec5c23c76
RaymondLoranger <Raymond.Loranger@bell.net>
2018-04-14 08:47:36 -0400

    0.1.1

---
+++ config/persist_course_ref.exs
@@ -0,0 +1,9 @@
+use Mix.Config
+
+config :hangman_text_client,
+  course_ref:
+    """
+    Based on the course [Elixir for Programmers](https://codestool.
+    coding-gnome.com/courses/elixir-for-programmers) by Dave Thomas.
+    """
+    |> String.replace("\n", "")
