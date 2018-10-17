4fd7f41d919093fe51404adbb10f5605aed5e7f5
RaymondLoranger <Raymond.Loranger@bell.net>
2018-10-16 20:28:58 -0400

    0.1.5

--- lib/hangman/text_client/summary.ex
+++ lib/hangman/text_client/summary.ex
@@ -8,29 +8,12 @@ defmodule Hangman.TextClient.Summary do
 
   @spec display(State.t()) :: State.t()
   def display(%State{tally: tally} = state) do
-    # IO.puts([
-    #   "\n",
-    #   "Word so far:  #{Enum.join(tally.letters, " ")}\n",
-    #   "Guesses left: #{tally.turns_left}\n"
-    # ])
     IO.puts("""
+
     Word so far:  #{Enum.join(tally.letters, " ")}
     Guesses left: #{tally.turns_left}
     """)
 
-    # IO.puts([
-    #   "\n",
-    #   "Word so far:  ",
-    #   io_list(tally.letters),
-    #   "\n",
-    #   "Guesses left: #{tally.turns_left}"
-    # ])
-
     state
   end
-
-  ## Private functions
-
-  # @spec io_list([String.codepoint()]) :: IO.chardata()
-  # defp io_list(letters), do: letters |> Enum.reduce([], &[&2 | [&1, " "]])
 end
