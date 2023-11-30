9a383c8dc5edf62a5446872df46d8aea62e399fe
RaymondLoranger <Raymond.Loranger@bell.net>
2023-11-29 21:16:05 -0500

    0.1.72

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hangman.Text.Client.Mixfile do
   def project do
     [
       app: :hangman_text_client,
-      version: "0.1.71",
+      version: "0.1.72",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Hangman Game",
